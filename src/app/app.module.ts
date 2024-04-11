757a34ade7c233053b25aa6d9159dd1b70f0734c
Joshua Debazar <debajoshua@gmail.com>
2024-04-11 21:57:56 +0000

    Reducers And Actions defined

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { MyCounterComponent } from './my-counter/my-counter.component';
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './count.reducer';
 
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [AppComponent],
+  imports: [BrowserModule,CommonModule, StoreModule.forRoot({count: counterReducer})],
+  declarations: [AppComponent, MyCounterComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
